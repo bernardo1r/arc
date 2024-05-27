9639b16b45371d82aa391cdc2a7aef833e639184
bernardo1r <bernardo@rezende.cc>
2024-05-27 05:03:31 -0300

    Updated main to use arc.Reader

--- cmd/arc.go
+++ cmd/arc.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"arc"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/klauspost/compress/zstd"
 	_ "github.com/mattn/go-sqlite3"
@@ -35,11 +38,31 @@ func fatalUsage() {
 func mustBeFolder(folderpath string) {
 	info, err := os.Stat(folderpath)
 	if err != nil {
-		log.Fatalln(info)
+		log.Fatalln(err)
 	}
+
 	if !info.IsDir() {
 		log.Fatalf("%s is not a folder\n", folderpath)
 	}
+}
+
+func createOrTruncateFolder(folderpath string) error {
+
+	err := os.Mkdir(folderpath, 0666)
+	if err == nil {
+		return nil
+	}
+
+	if errors.Is(err, os.ErrExist) {
+		err = os.RemoveAll(folderpath)
+		if err != nil {
+			return err
+		}
+
+		err = os.Mkdir(folderpath, 0666)
+	}
+
+	return err
 }
 
 func main() {
@@ -62,6 +85,7 @@ func main() {
 	folderPath := filepath.Clean(flag.Arg(0))
 	mustBeFolder(folderPath)
 
+	start := time.Now()
 	builder, err := arc.NewBuilder(
 		filepath.Base(folderPath)+dbExtesion,
 		dbArgs,
@@ -69,9 +93,37 @@ func main() {
 		arc.WithPassword([]byte("hello motto")),
 	)
 	checkError(err)
-	defer func() {
-		checkError(builder.Close())
-	}()
+
 	err = builder.InsertDir(folderPath)
 	checkError(err)
+
+	err = builder.Close()
+	checkError(err)
+	tot := time.Since(start)
+	fmt.Printf("Time to write to container: %v\n\n", tot)
+
+	newFolderPath := filepath.Base(folderPath) + "_opened"
+	err = createOrTruncateFolder(newFolderPath)
+	checkError(err)
+
+	start = time.Now()
+	reader, err := arc.NewReader(
+		filepath.Base(folderPath)+dbExtesion,
+		dbArgs,
+		[]byte("hello motto"),
+	)
+	checkError(err)
+
+	files, err := reader.Files()
+	checkError(err)
+
+	for filename, header := range files {
+		filename = filepath.Join(newFolderPath, filename)
+		fmt.Printf("Putting %s\n", filename)
+		err = reader.ReadToFile(header.Id, filename)
+		checkError(err)
+	}
+	tot = time.Since(start)
+
+	fmt.Printf("Time to write files from container: %v\n", tot)
 }
